a7707c9509ec1e40d9b830f25983d5d0166e218d
tstevens <tstevens@punchey.com>
2022-02-16 11:55:49 -0500

    Updated main page

--- VanLife/App.js
+++ VanLife/App.js
@@ -1,16 +1,51 @@
 import React from 'react';
-import {StyleSheet, Button, View} from 'react-native';
+import {StyleSheet, Pressable, Text, View} from 'react-native';
 import Header from './Components/header';
 
 export default function App() {
   return (
     <View>
-      <Header />
-      <Button color="#696969" title="Maps"></Button>
-      <Button color="#696969" title="Top Rated Routes"></Button>
-      <Button color="#696969" title="Get Ready for the Road"></Button>
-      <Button color="#696969" title="Connect"></Button>
-      <Button color="#696969" title="Forums"></Button>
+      <Header style={styles.header} />
+      <View style={styles.container}>
+        <Pressable style={styles.pressable}>
+          <Text style={styles.text}>{'Maps'}</Text>
+        </Pressable>
+        <Pressable style={styles.pressable}>
+          <Text style={styles.text}>{'Top Routes'}</Text>
+        </Pressable>
+        <Pressable style={styles.pressable}>
+          <Text style={styles.text}>{'Preperation'}</Text>
+        </Pressable>
+        <Pressable style={styles.pressable}>
+          <Text style={styles.text}>{'Connect'}</Text>
+        </Pressable>
+        <Pressable style={styles.pressable}>
+          <Text style={styles.text}>{'Forums'}</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    paddingLeft: 65,
+  },
+  pressable: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    padding: 6,
+    height: 60,
+    width: '80%',
+    backgroundColor: '#696969',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});

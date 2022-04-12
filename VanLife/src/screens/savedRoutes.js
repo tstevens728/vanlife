bb3c26f8e638887849d6c9d85c5bc1c7c34e3f24
tstevens <tstevens@punchey.com>
2022-04-12 14:11:59 -0400

    added saved routes page

--- VanLife/src/screens/savedRoutes.js
+++ VanLife/src/screens/savedRoutes.js
@@ -1,15 +1,62 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, Button, View, ImageBackground} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  Text,
+  FlatList,
+  View,
+} from 'react-native';
 import Header from '../components/header';
+
+const DATA = [
+  {
+    id: '1',
+    title: 'NewYork to Miami',
+  },
+  {
+    id: '2',
+    title: 'Maine to Los Angeles',
+  },
+  {
+    id: '3',
+    title: 'Arizona to Maine',
+  },
+  {
+    id: '4',
+    title: 'GrandCanyon to Niagra Falls',
+  },
+  {
+    id: '5',
+    title: 'Boston to Chicago',
+  },
+  {
+    id: '6',
+    title: 'Arizona to Washington D.C.',
+  },
+];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 export class SavedRoutes extends Component {
   render() {
+    const renderItem = ({item}) => <Item title={item.title} />;
+
     return (
       <View>
         <Header style={styles.header} />
-        <View style={styles.container}>
-          <Text style={styles.text}> Saved Routes:</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+          <Text style={styles.text}> Saved Routes</Text>
+          <FlatList
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={item => item.id}
+          />
+        </SafeAreaView>
       </View>
     );
   }
@@ -33,5 +80,15 @@ const styles = StyleSheet.create({
     letterSpacing: 0.25,
     color: '#FFE4C4',
     padding: 10,
+  },
+  item: {
+    backgroundColor: '#FFE4C4',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 15,
+    fontWeight: 'bold',
   },
 });
